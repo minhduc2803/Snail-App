9edf00b5b86bbbe506cf764e209eb4b083d87dbc
minhduc2803 <hontomoni@gmail.com>
2020-09-29 17:52:06 +0700

    learn grpc

---
+++ src/ClientSide/build/precache-manifest.2f9467b76b9e52d110801b1863b77424.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8129cc277095082381b2",
+    "url": "/static/css/main.280c8e58.chunk.css"
+  },
+  {
+    "revision": "8129cc277095082381b2",
+    "url": "/static/js/main.8129cc27.chunk.js"
+  },
+  {
+    "revision": "d9b9bd54d79f03db0bf7",
+    "url": "/static/js/1.d9b9bd54.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "932dbb73bdd90e115ac82ac0efe1c601",
+    "url": "/index.html"
+  }
+];

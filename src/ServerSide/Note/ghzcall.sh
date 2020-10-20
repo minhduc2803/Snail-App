025cf789477a3b9048e9dafea963fd5921c2cd55
minhduc2803 <hontomoni@gmail.com>
2020-10-20 17:29:00 +0700

    benmark with ghz, monitor with prometheus,grafana

--- src/ServerSide/Note/ghzcall.sh
+++
@@ -1,7 +0,0 @@
-~/MinhDuc/module-10/SnailProject/src/ServerSide/Note/ghz --insecure \
-  --proto ../src/main/proto/fintech.proto \
-  --call fintech.FintechService.transfer \
-  -n 100000 -c 20 \
-  -d '{"sender_id": 8, "receiver_id": 4, "amount": 10, "message":"hello", "password":"suneo" }' \
-  -m '{"Authorization":"Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1IiwiZXhwIjo0NjIyNDcwNDIyfQ.ZXtvktZytH4bK2aaokJTWyxKofpyu8hzyjL7OhljG54"}' \
-  0.0.0.0:8000
